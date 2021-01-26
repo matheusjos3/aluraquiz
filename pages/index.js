2bbe1083c7d945936fc2a6b118d0f0f35555eb69
Matheus <matheusdc98@gmail.com>
2021-01-26 13:24:26 -0300

    Aula 01

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,56 @@
 import styled from 'styled-components'
+import db from '../db.json';
+import Widget from '../src/Components/Widget';
+import QuizBackground from '../src/Components/QuizBackground';
+import QuizLogo from '../src/Components/QuizLogo';
+import Footer from '../src/Components/Footer';
+import GitHubCorner from '../src/Components/GitHubCorner';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+// const BackgroundImage = styled.div `
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>The legend of Zelda</h1>
+          </Widget.Header>
+          <Widget.content>
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.content>
+        </Widget>
+        <Widget>
+          <Widget.content>
+            <h1>The legend of Zelda</h1>
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/matheusjos3" />
+    </QuizBackground>
+  )
 }
